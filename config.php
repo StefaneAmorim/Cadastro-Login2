2d9554c72c8b4dfa45ade352f4abdb56cc7a12f9
StefaneAmorim <104101680+StefaneAmorim@users.noreply.github.com>
2023-11-01 20:04:28 -0300

    Create config.php

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+    define('HOST', 'localhost');
+    define('USER', 'root');
+    define('PASS', '');
+    define('BASE', 'projeto_login');
+
+    $conn = new MySQLi(HOST, USER, PASS, BASE);
+
+?>
